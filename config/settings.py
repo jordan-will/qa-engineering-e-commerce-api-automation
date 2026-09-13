782ac262542250f79865dab6b1a930aec8cbf28c
Jordan Willian <jordan.willian.mp@gmail.com>
2026-09-13 12:22:37 -0300

    feat: add API credentials cofiguration

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,3 +7,5 @@ load_dotenv()
 BASE_URL = os.getenv("BASE_URL", "https://api.example.com")
 REQUEST_TIMEOUT = int(os.getenv("REQUEST_TIMEOUT", "5"))
 ENVIRONMENT = os.getenv("ENVIRONMENT", "dev")
+API_USERNAME = os.getenv("API_USERNAME", "")
+API_PASSWORD = os.getenv("API_PASSWORD", "")
